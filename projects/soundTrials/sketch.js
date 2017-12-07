8323ad26c07b27a1834d7116e1d6e6b99ffcc3b8
isaacwellish <isaacwellish@gmail.com>
2017-12-07 00:47:31 -0500

    Meditation Visualizer Update
    Added visuals and better instructions for meditation project

--- projects/soundTrials/sketch.js
+++ projects/soundTrials/sketch.js
@@ -9,6 +9,9 @@ var d3;
 var e2;
 var g1;
 var g2;
+var drops = [];
+
+var instructions;
 
 
 function preload(){
@@ -29,38 +32,49 @@ function preload(){
 }
 
 function keyPressed(){
+
+
+
 	if(keyCode == 90){//z
 		c1.play();
 		c1.setVolume(1.0)
+		instructions.remove();
 	}
 
 	else if(keyCode == 88){ //x
 		g1.play();
+		instructions.remove();
 
 	}
 
 	else if(keyCode == 67){ //c
 		c2.play();
+		instructions.remove();
 
 	}
 	else if(keyCode == 86){//v
 		e2.play();
+		instructions.remove();
 
 	}
 	else if(keyCode == 66){//b
 		g2.play();
+		instructions.remove();
 
 	}
 	else if(keyCode == 78){//n
 		b2.play();
+		instructions.remove();
 
 	}
 	else if(keyCode == 77){//m
 		c3.play();
+		instructions.remove();
 
 	}
 	else if(keyCode == 188){//,
 		d3.play();
+		instructions.remove();
 
 	}
 
@@ -74,10 +88,31 @@ function keyPressed(){
 
 }
 
+
+
+
+function styleInstructions(){
+
+	tCol = color(87, 118, 142, 200);
+	bCol = color(87, 118, 142, 50);
+
+	instructions = select('.how');
+	instructions.style("font-size", "24px");
+    instructions.style("color", tCol);
+    instructions.style("background-color", bCol);
+    instructions.style("padding", "8px");
+    instructions.position(width/2.5, height/5); // name Title positioning
+
+}
+
+
 function setup() {
-	createCanvas(2880, 1800);
-	image(img, 0, 0);
-	//allINeed.play();
+	createCanvas(windowWidth, windowHeight);
+  	for (var i = 0; i < 500; i++) {
+    	drops[i] = new Drop();
+  	}
+
+  	styleInstructions();
 
 	rain.play();
 	rain.setVolume(0.4);
@@ -87,9 +122,12 @@ function setup() {
 function draw() {
 	//background(255, 150);
 
-	fill(0);
-	noStroke();
-	rect(1280, 0, 160, 1800);
-	rect(0,720,1280,100);
+	background(155, 211, 255);
+  	for (var i = 0; i < drops.length; i++) {
+    drops[i].fall();
+    drops[i].show();
+  	}
+
+  	
 
 }
